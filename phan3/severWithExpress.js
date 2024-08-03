8317c32ecd62c9a483d56351617e91d437b6a4ee
= <=>
2024-08-03 15:38:32 +0700

    to 14m53s

--- phan3/severWithExpress.js
+++ phan3/severWithExpress.js
@@ -53,69 +53,48 @@ app.use(express.json())
 // express.static() là một middleware tích hợp 
 // trong Express, được sử dụng để phục vụ các tệp 
 // tĩnh từ một thư mục cụ thể.
-app.use(express.static(path.join(__dirname, '/public')))
+app.use('/',express.static(path.join(__dirname, '/public')))
+app.use('/subdir',express.static(path.join(__dirname, '/public')))
 
-// define route xử lí cá request GET đến đường 
-// dẫn '/' (trang chủ). Hàm callback sẽ được gọi
-// mỗi khi có request đến dường dẫn này
+// use Express Router
+// Điều này có nghĩa là bất kỳ yêu cầu nào bắt đầu 
+// với /subdir sẽ được xử lý bởi các tuyến đường được 
+// định nghĩa trong module ./routes/subdir
+app.use('/', require('./routes/root'))
+app.use('/subdir', require('./routes/subdir'))
+app.use('/employees', require('./routes/api/employees'))
 
-// phần (.html)? đã giải thích ở dưới
-// ^: đế bắt đầu chuỗi, $ kết thúc chuỗi, / là đại
-// diện cho dấu gách chép trong url
-// ^/$: nghĩa là nó phải kết thúc bằng dấu gách chéo
-// | toán tử or trong regex nghĩa là nó cho khớp
-// vs 1 trong 2 pattern
 
-app.get('^/$|/index(.html)?', (req, res) => {
-    // cách 1: res.sendFile('./views/index.html', {root: __dirname})
-    // cách 2:
-    res.sendFile(path.join(__dirname, 'views', 'index.html'))
-})
 
-// /new-page là phần tính còn 
-// (.html)? là 1 regex: phần (.html) là 1 capturing group
-// chứa hmtl. phần ? làm cho phần trước đó là optional.
-// nghĩa là khi kết hợp lại (.html)? thì phần html có
-// thể có hoặc không
-app.get('/new-page(.html)?', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'new-page.html'))
-})
+// // Route handlers 
+// app.get('/hello(.html)?', (req, res, next) => {
+//     console.log('attempted to load hello.html')
+//     // next() chuyển qua middleware function tiếp theo
+//     // trong stack cụ thể là function phía dưới và
+//     // trả về client Hello World!. Hoặc có cách
+//     // dưới
+//     next()
+// }, (req, res) => {
+//     res.send('Hello World!')
+// })
 
-app.get('/old-page(.html)?',(req, res) => {
-    // 302 by default
-    // but i want 301 because 301 is permanently
-    res.redirect(301, '/new-page(.html)?') 
-})
+// // chaining route handlers
+// const one = (req, res, next) => {
+//     console.log('one')
+//     next()
+// }
 
-// Rount handlers 
-app.get('/hello(.html)?', (req, res, next) => {
-    console.log('attempted to load hello.html')
-    // next() chuyển qua middleware function tiếp theo
-    // trong stack cụ thể là function phía dưới và
-    // trả về client Hello World!. Hoặc có cách
-    // dưới
-    next()
-}, (req, res) => {
-    res.send('Hello World!')
-})
+// const two = (req, res, next) => {
+//     console.log('two')
+//     next()
+// }
 
-// chaining route handlers
-const one = (req, res, next) => {
-    console.log('one')
-    next()
-}
+// const three = (req, res, next) => {
+//     console.log('three')
+//     res.send('Finished')
+// }
 
-const two = (req, res, next) => {
-    console.log('two')
-    next()
-}
-
-const three = (req, res, next) => {
-    console.log('three')
-    res.send('Finished')
-}
-
-app.get('/chain(.html)?', [one, two, three])
+// app.get('/chain(.html)?', [one, two, three])
 
 // /*: là regex khớp với all route sau dấu gạch chéo
 // Nghĩa là, mọi yêu cầu GET đến bất kỳ URL nào mà 
